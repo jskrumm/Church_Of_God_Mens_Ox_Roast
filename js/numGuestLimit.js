3287afe54c9c6ddfe9b746955dcad1b1e8840da9
Josh Krumm <jskrumm@gmail.com>
2013-06-01 23:14:32 -0400

    Updates for registration.

--- js/numGuestLimit.js
+++ js/numGuestLimit.js
@@ -7,20 +7,17 @@ var ClickCounterViewModel = function () {
         self.guestCount(self.guestCount() + 1);
     };
 
+    self.removeField = function () {
+        self.guestCount(self.guestCount() - 1);
+    };
+
     self.hasClickedTooManyTimes = ko.computed(function () {
         return self.guestCount() >= 19;
     }, self);
+
+    self.hasMoreThanOneGuest = ko.computed(function () {
+        return self.guestCount() >= 2;
+    }, self);
 };
 
-/*
-    ko.bindingHandlers.initializeValue = {
-    init: function(element, valueAccessor) {
-        valueAccessor()(element.getAttribute('value'));
-    },
-    update: function(element, valueAccessor) {
-        var value = valueAccessor();
-        element.setAttribute('value', ko.utils.unwrapObservable(value))
-    }
-};
-*/
 ko.applyBindings(new ClickCounterViewModel());
--- js/registration.js
+++ js/registration.js
@@ -6,15 +6,29 @@ var getTotal = function (qty, price) {
     } else {
         return total;
     }
-};
+},
+formatCurrency = function (value) {
+    return "$" + value;
+},
+RegistrationVM = function () {
+    var self = this,
+        now = new Date(),
+        month = now.getMonth() + 1,
+        day = now.getDate(),
+        monthDay = month.toString() + day.toString(),
+        twoDayPrice = 65;
 
-var RegistrationVM = function () {
-    var self = this;
+    if (monthDay <= 815) {
+        twoDayPrice = 55;
+    }
 
     self.numAttTwoDaysQty = ko.observable(parseInt($("#num-att-two-day").attr("data-initalVal")));
-    self.numAttTwoDaysPrice = ko.observable(65);
+    self.numAttTwoDaysPrice = ko.observable(twoDayPrice);
     self.numAttTwoDaysTotal = ko.computed(function () {
         return getTotal(self.numAttTwoDaysQty(), self.numAttTwoDaysPrice());
+    });
+    self.numAttTwoDaysTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numAttTwoDaysTotal());
     });
 
     self.numAttOneDayQty = ko.observable(0);
@@ -22,11 +36,17 @@ var RegistrationVM = function () {
     self.numAttOneDayTotal = ko.computed(function () {
         return getTotal(self.numAttOneDayQty(), self.numAttOneDayPrice());
     });
+    self.numAttOneDayTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numAttOneDayTotal());
+    });
 
     self.numAttUnderageQty = ko.observable(0);
     self.numAttUnderagePrice = ko.observable(25);
     self.numAttUnderageTotal = ko.computed(function () {
         return getTotal(self.numAttUnderageQty(), self.numAttUnderagePrice());
+    });
+    self.numAttUnderageTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numAttUnderageTotal());
     });
 
     self.numGolfingQty = ko.observable(0);
@@ -34,11 +54,17 @@ var RegistrationVM = function () {
     self.numGolfingTotal = ko.computed(function () {
         return getTotal(self.numGolfingQty(), self.numGolfingPrice());
     });
+    self.numGolfingTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numGolfingTotal());
+    });
 
     self.numPaintballingQty = ko.observable(0);
     self.numPaintballingPrice = ko.observable(5);
     self.numPaintballingTotal = ko.computed(function () {
         return getTotal(self.numPaintballingQty(), self.numPaintballingPrice());
+    });
+    self.numPaintballingTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numPaintballingTotal());
     });
 
     self.numFishingQty = ko.observable(0);
@@ -46,10 +72,13 @@ var RegistrationVM = function () {
     self.numFishingTotal = ko.computed(function () {
         return getTotal(self.numFishingQty(), self.numFishingPrice());
     });
+    self.numFishingTotalFormated = ko.computed(function () {
+        return formatCurrency(self.numFishingTotal());
+    });
 
     self.grandTotal = ko.computed(function () {
-        return self.numFishingTotal() + self.numPaintballingTotal() + self.numGolfingTotal() + self.numAttUnderageTotal() + self.numAttOneDayTotal() + self.numAttTwoDaysTotal();
+        return formatCurrency(self.numFishingTotal() + self.numPaintballingTotal() + self.numGolfingTotal() + self.numAttUnderageTotal() + self.numAttOneDayTotal() + self.numAttTwoDaysTotal());
     });
 };
  
-ko.applyBindings(new RegistrationVM(),document.getElementById('eventInfo')); 
+ko.applyBindings(new RegistrationVM(), document.getElementById('eventInfo')); 
