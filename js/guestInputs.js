8501ac6ab454ce1ff6a0eb14b344eed464513cfe
Josh Krumm <jskrumm@gmail.com>
2014-01-19 12:06:53 -0500

    New guest registration complete.

--- js/guestInputs.js
+++ js/guestInputs.js
@@ -3,19 +3,26 @@ function Guest(firstName, lastName) {
     this.lastName = lastName;
 }
 
-function guestVm() {
+function guestVM() {
     var self = this;
 
     self.guest = ko.observableArray([]);
+    self.guestCount = ko.observable(parseInt($("#guest-count").attr("data-initalVal")));
 
     self.addGuest = function () {
         self.guest.push(new Guest($("#guest_firstname").val(), $("#guest_lastname").val()));
+        self.guestCount(self.guestCount() + 1);
     }
 
     self.removeGuest = function () {
         self.guest.remove(this);
+        self.guestCount(self.guestCount() - 1);
     }
+
+    self.hasClickedTooManyTimes = ko.computed(function () {
+        return self.guestCount() >= 19;
+    }, self);
 };
 
-ko.applyBindings(new guestVm(), $("#guests")[0]);
+ko.applyBindings(new guestVM(), $("#guests")[0]);
 
